649e738eadb5070f06274ede64e578fa04de30b5
sananasgarov <prog58816@gmail.com>
2026-01-06 11:31:44 +0000

    listdivion

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    c = []
+    for i in range(list_length):
+        try:
+            q = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            q = 0
+        except TypeError:
+            print("wrong type")
+            q = 0
+        except IndexError:
+            print("out of range")
+            q = 0
+        finally:
+            c.append(q)
+    return c
