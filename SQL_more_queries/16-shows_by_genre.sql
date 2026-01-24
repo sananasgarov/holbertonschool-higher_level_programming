068e00d30a881453fe6405a3e4d791511e9207af
Adsananasgarov <prog58816@gmail.com>
2026-01-24 12:32:42 +0000

    SQLFINISH

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- Lists all shows and all genres linked to that show from hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, displays NULL.
+-- Results are sorted by show title and genre name.
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
